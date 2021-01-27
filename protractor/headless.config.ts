b78ade7a34dcd63ebf05f321cac25d1a4677b766
mpgarciav <51894580+mpgarciav@users.noreply.github.com>
2021-01-27 14:17:08 -0500

    Chrome headless

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -5,8 +5,15 @@ export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/google.spec.js' ],
   SELENIUM_PROMISE_MANAGER: false,
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
-  }
+  },
+
 };
