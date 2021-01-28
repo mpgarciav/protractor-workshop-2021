9d53b06025f89bcf76aa1cece756933e50897aca
Pau <magarciave@unal.edu.co>
2021-01-28 12:37:06 -0500

    Separate test into different describes

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -11,20 +11,19 @@ import {
   OrderSummaryPage,
   AddressStepPage } from '../src/page';
 
+describe('Open web page', () => {
+  it('then should open a web page', async () => {
+    await browser.get('http://automationpractice.com/');
+  });
+});
+
 describe('Buy a t-shirt', () => {
   const menuContentPage: MenuContentPage = new MenuContentPage();
   const productListPage: ProductListPage = new ProductListPage();
   const productAddedModalPage: ProductAddedModal = new ProductAddedModal();
   const summaryStepPage: SummaryStepPage = new SummaryStepPage();
-  const signInStepPage: SignInStepPage = new SignInStepPage();
-  const addressStepPage: AddressStepPage = new AddressStepPage();
-  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
-  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
 
   it('then should be bought a t-shirt', async () => {
-    await browser.get('http://automationpractice.com/');
     await menuContentPage.goToTShirtMenu();
     // await(browser.sleep(3000));
     await productListPage.goToProduct();
@@ -33,21 +32,36 @@ describe('Buy a t-shirt', () => {
     // await(browser.sleep(3000));
     await summaryStepPage.goToCheckout();
     // await(browser.sleep(3000));
-
+  });
+});
+describe('Log into the app', () => {
+  const signInStepPage: SignInStepPage = new SignInStepPage();
+  it('then should be logged into the app', async () => {
     await signInStepPage.fillForm('aperdomobo@gmail.com', 'WorkshopProtractor');
     // await(browser.sleep(3000));
+  });
+});
 
+describe('Select address', () => {
+  const addressStepPage: AddressStepPage = new AddressStepPage();
+  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+  it('then should select the address', async () => {
     await addressStepPage.selectAddress();
     // await(browser.sleep(3000));
-
     await shippingStepPage.accept();
     // await(browser.sleep(3000));
+  });
 
+});
+describe('Bank payment', () => {
+  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+  it('then should the bank pay', async () => {
     await paymentStepPage.selectPaymentMethod();
     // await(browser.sleep(3000));
     await bankPaymentPage.confirmOrder();
     // await(browser.sleep(3000));
-
     await expect(orderSummaryPage.result())
       .toBe('Your order on My Store is complete.');
   });
